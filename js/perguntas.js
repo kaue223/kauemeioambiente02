69635e9dfe525bb83e8e71c5006f01edff89945e
kaue223 <126253414+kaue223@users.noreply.github.com>
2024-08-26 23:06:32 -0300

    Site concluído

---
+++ js/perguntas.js
@@ -0,0 +1,161 @@
+export const perguntas = [
+    {
+        enunciado: "Assim que você chega em casa vindo da escola, se pergunta: qual o primeiro passo?",
+        alternativas: [
+            {
+                texto: "Separar o lixo",
+                afirmacao: [
+                    "Ao separar o lixo, ajudou no descarte correto para cada tipo de resíduo, reduzindo a quantidade de lixo na natureza.",
+                    "Separar o lixo adequadamente facilitou seu descarte, sem prejudicar a natureza."
+                ],
+                proxima: 1,
+            },
+            {
+                texto: "Reutilizar o lixo reciclável",
+                afirmacao: [
+                    "Em vez de simplesmente jogar o lixo fora, criou algo legal com o que achou:",
+                    "Tirou proveito de coisas que seriam jogadas fora e criou algo legal:"
+                ],
+                proxima: 2,
+            },
+        ]
+    },
+    {
+        enunciado: "Ao ver entulhos tampando os bueiros e impedindo o escoamento de água da rua onde mora, qual atitude tomar?",
+        alternativas: [
+            {
+                texto: "Recolher o lixo das ruas",
+                afirmacao: [
+                    "Conseguiu limpar a rua e os bueiros voltaram a escoar a água.",
+                    "A rua ficou limpa e sem entulhos."
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "Consientizar os vizinhos sobre o descarte de lixo nas ruas",
+                afirmacao: [
+                    "Os vizinhos do seu bairro passaram a cuidar mais da rua, mantendo ela sempre limpa.",
+                    "Conscientizou seus vizinhos que colocaram um latão de lixo na rua onde mora para que não houvesse mais lixo no chão."
+                ],
+                proxima: 3,
+            },
+        ]
+    },
+    {
+        enunciado: "Vasculhando o lixo em busca de reutilizar algo encontrou algumas garrafas pet e uns vidros de conserva. O que podemos criar?",
+        alternativas: [
+            {
+                texto: "Fazer uma horta vertical com as garrafas pet encontradas",
+                afirmacao: [
+                    "fez uma hortinha vertical para sua casa utilizando as garrafas pet e inspirou parentes e vizinhos a fazer o mesmo.",
+                    "a hortinha vertical feita com garrafas pet fez sucesso no bairro e ajudou a reduzir a quantidade desses plásticos no meio ambiente.",
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "Criar luminárias decorativas com os vidros de conserva",
+                afirmacao: [
+                    "fez luminárias decorativas utilizando vidros de conserva e deixou sua casa mais bonita de maneira sustentável.",
+                    "luminárias decorativas, foram uma escolha sustentável que inspiraram mais pessoas a reutilizar o lixo reciclável.",
+                ],
+                proxima: 3,
+            },
+        ]
+    },
+    {
+        enunciado: "Qual ação você pode adotar na sua cidade para diminuir os gases causadores do efeito estufa?",
+        alternativas: [
+            {
+                texto: "Plantar árvores em áreas urbanas e fazer um reflorestamento em áreas desmatadas",
+                afirmacao: [
+                    "Criou um grupo de reflorestadores que fizeram a paisagem ficar mais verde em meio aos prédios, melhorando a qualidade de vida dos habitantes da cidade."
+                ],
+                proxima: 4,
+            },
+            {
+                texto: "promover o uso de bicicletas e transportes públicos como meio de locomoção",
+                afirmacao: [
+                    "A cidade adotou o ciclismo como forma de locomoção, assim como transportes públicos, diminuindo efetivamente a emissão dos gases causadores do efeito estufa e prejudiciais para a saúde humana",
+                    "Houve melhorias no transporte público e o ciclismo se tornou hábito entre muitos habitantes da cidade, ajudando a reduzir drasticamente a emissão de gás carbonico na atmosfera."
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "A escola decide fazer mudanças para contribuir com a redução de papel descartado e pediu sua ajuda. O que podemos implementar?",
+        alternativas: [
+            {
+                texto: "Promover o uso de materiais escolares feitos a partir de materiais reciclados",
+                afirmacao: [
+                    "Usar materiais reciclados incentivou a escola a adotar práticas mais sustentáveis e reduziu os resíduos na comunidade escolar.",
+                    "Utilizar materiais reciclados ajudou a aumentar a conscientização sobre reciclagem entre os alunos, promovendo uma maior responsabilidade ambiental na comunidade escolar."
+                ],
+                proxima: 5,
+            },
+            {
+                texto: "Utilizar dispositivos eletrônicos para anotações e leituras em vez de papel",
+                afirmacao: [
+                    "Digitalizar as anotações reduziu os custos com papel e impressões na escola, economizando recursos financeiros e ambientais para a instituição.",
+                    "Digitalizar reduziu o uso de papel na escola, promovendo a sustentabilidade e inspirando práticas ambientais entre os alunos."
+                ],
+                proxima: 6,
+            },
+        ]
+    },
+    {
+        enunciado: "Como você pode apoiar a sustentabilidade ao escolher novos materiais escolares?",
+        alternativas: [
+            {
+                texto: "Optando por produtos com certificação de sustentabilidade.",
+                afirmacao: [
+                    "Optar por produtos com certificação de sustentabilidade assegura que os materiais foram produzidos com práticas responsáveis, apoiando a preservação ambiental e promovendo o consumo consciente."
+                ],
+                proxima: 7,
+            },
+            {
+                texto: "Participando de iniciativas de troca ou doação de materiais escolares.",
+                afirmacao: [
+                    "Participar de iniciativas de troca ou doação de materiais escolares reduziu o desperdício e a demanda por novos produtos, incentivando uma economia circular e práticas sustentáveis na comunidade escolar."
+                ],
+                proxima: 7,
+            },
+        ]
+    },
+    {
+        enunciado: "Como você pode garantir que o uso de dispositivos eletrônicos seja eficiente e sustentável?",
+        alternativas: [
+            {
+                texto: "Ajustando as configurações de energia para otimizar o uso da bateria",
+                afirmacao: [
+                    "Ajustar as configurações de energia dos dispositivos reduziu o consumo de bateria e prolongou a vida útil dos aparelhos, contribuindo para uma abordagem mais sustentável ao uso de tecnologia."
+                ],
+                proxima: 7,
+            },
+            {
+                texto: "Utilizando aplicativos de leitura e anotação que suportem modos de economia de energia.",
+                afirmacao: [
+                    "Utilizando aplicativos de leitura e anotação que suportem modos de economia de energia diminuiu o impacto ambiental e melhorou a eficiência no uso dos dispositivos eletrônicos."
+                ],
+                proxima: 7,
+            },
+        ]
+    },
+    {
+        enunciado: "Como você pode expandir a divulgação de boas práticas de sustentabilidade para alcançar um público mais amplo além da comunidade local?",
+        alternativas: [
+            {
+                texto: "Criando um blog ou canal de mídia social dedicado a práticas sustentáveis",
+                afirmacao: [
+                    "Seu blog viralizou no Brasil e conseguiu alcançar o país de norte a sul, muitos brasileiros e grandes empresas tomaram as atitudes corretas para preservar o meio ambiente"
+                ],
+            },
+            {
+                texto: "Participando de conferências e eventos regionais ou nacionais sobre meio ambiente e sustentabilidade",
+                afirmacao: [
+                    "Participou de muitos eventos debatendo sobre boas práticas e inspirou muitos brasileiros, assim como coorporações e indústrias a mudarem seu comportamento em relação a preservação da nossa natureza."
+                ],
+            },
+        ]
+    }
+];
